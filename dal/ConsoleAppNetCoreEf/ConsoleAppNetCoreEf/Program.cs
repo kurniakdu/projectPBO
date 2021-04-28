292c11c73091d4683366b022766f8e3a9ce448e1
Kurnia Dwi <kurniakdu@gmail.com>
2021-04-29 01:27:55 +0700

    input dulu biar aman

---
+++ dal/ConsoleAppNetCoreEf/ConsoleAppNetCoreEf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppNetCoreEf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
