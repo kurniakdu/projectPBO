01923fe7dccc865f9227bfad8f1e43872f46d417
Kurnia Dwi <kurniakdu@gmail.com>
2021-04-29 06:40:57 +0700

    kesadaran diri

--- dal/ConsoleAppNetCoreEf/ConsoleAppNetCoreEf/Program.cs
+++ dal/ConsoleAppNetCoreEf/ConsoleAppNetCoreEf/Program.cs
@@ -8,5 +8,10 @@ namespace ConsoleAppNetCoreEf
         {
             Console.WriteLine("Hello World!");
         }
+        //sebenarnya saya gak yakin kenapa milih orm
+        //saya jadinya tidak mengisi databasenya dahulu
+        //mungkin karena saya belum sepenuhnya yakin dan paham :'
+        //saya juga menyayangkan keadaan saya yang menjadi deadliner dalam mengerjakan tugas ini
+        //maafkan saya Pak, harusnya saya prepare lebih lama supaya bisa ngulik lebih jauh lagi
     }
 }
